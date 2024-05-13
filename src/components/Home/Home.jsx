1647272a4167d899a166704854a960d2be49ab2c
renatakov <renatakov1010@gmail.com>
2024-05-13 16:19:30 +0200

    1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import s from "./Home.module.scss"
 
 const Home = () => {
     return (
+        <>
         
             <header>
                 <img src="https://corkerro.github.io/volumenzeit/img/logo.svg" alt="" />
@@ -17,6 +18,19 @@ const Home = () => {
                     </ul>
                 </nav>
             </header>
+            <section className={s.home}>
+                <aside>
+                    <p>follow us</p>
+                    
+                </aside>
+                <div className={s.homeBlock}>
+                <h1>Find your dream watch</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim facilisi elementum commodo ipsum.<br/> Aenean aenean adipiscing lect</p>
+                <button>Design your watch</button>
+                </div>
+                <img src="https://corkerro.github.io/volumenzeit/img/main/watch.png" alt="" />
+            </section>
+        </>
         
 
 
