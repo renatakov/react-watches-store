3a21e3bd68d96fa3762a1905f93cb26e1e228497
renatakov <renatakov1010@gmail.com>
2024-05-14 18:52:38 +0200

    1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,15 +20,33 @@ const Home = () => {
             </header>
             <section className={s.home}>
                 <aside>
+
+                        <ul className={s.home_socialList}>
+                            <li>
+                            <a href="#1"><img src="https://corkerro.github.io/volumenzeit/img/main/pinterest.svg" alt="" /></a>
+
+                            </li>
+                            <li>
+
+                        <a href="#4"><img src="https://corkerro.github.io/volumenzeit/img/main/facebook.svg" alt="" /></a>
+                            </li>
+                            <li>
+                            <a href="#2"><img src="	https://corkerro.github.io/volumenzeit/img/main/twitter.svg" alt="" /></a>
+
+                            </li>
+                            <li>
+
+                        <a href="#3"><img src="https://corkerro.github.io/volumenzeit/img/main/instagram.svg" alt="" /></a>
+                            </li>
+                    </ul>
                     <p>follow us</p>
-                    
                 </aside>
                 <div className={s.homeBlock}>
                 <h1>Find your dream watch</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim facilisi elementum commodo ipsum.<br/> Aenean aenean adipiscing lect</p>
                 <button>Design your watch</button>
                 </div>
-                <img src="https://corkerro.github.io/volumenzeit/img/main/watch.png" alt="" />
+                <img className={s.homeBlock_img} src="https://corkerro.github.io/volumenzeit/img/main/watch.png" alt="" />
             </section>
         </>
         
